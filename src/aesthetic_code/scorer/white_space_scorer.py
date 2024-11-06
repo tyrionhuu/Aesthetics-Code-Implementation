4075f21f7a635dda1da3e41ef6eefb3946b74929
Tianyu Hu <tyrionhuu@gmail.com>
2024-11-06 09:34:50 +0800

    margin whhite space add and passed unit test

--- src/aesthetic_code/scorer/white_space_scorer.py
+++ src/aesthetic_code/scorer/white_space_scorer.py
@@ -1,26 +1,35 @@
 from pptx.slide import Slide
 from pptx.util import Length
+
 from aesthetic_code.utils import unit_conversion
+
 
 class MarginWhiteSpaceScorer:
     """
     This class is responsible for scoring the white space in the margins of a slide.
     """
-    def __init__(self, slide: Slide, slide_width: Length, slide_height: Length, measurement_unit: str = "pt"):
+
+    def __init__(
+        self,
+        slide: Slide,
+        slide_width: Length,
+        slide_height: Length,
+        measurement_unit: str = "pt",
+    ):
         self._slide = slide
         self._measurement_unit = measurement_unit
         self._width = unit_conversion(slide_width, self._measurement_unit)
         self._height = unit_conversion(slide_height, self._measurement_unit)
         self._margin_threshold = {
-            'horizontal': (0.1 * self._width, 0.3 * self._width),
-            'vertical': (0.1 * self._height, 0.3 * self._height)
+            "horizontal": (0.1 * self._width, 0.3 * self._width),
+            "vertical": (0.1 * self._height, 0.3 * self._height),
         }
-        
+
     def _get_bounding_box(self) -> dict:
-        leftmost_x = 0
-        rightmost_x = self._width
-        topmost_y = 0
-        bottommost_y = self._height
+        leftmost_x = self._width
+        rightmost_x = 0.0
+        topmost_y = self._height
+        bottommost_y = 0.0
         for shape in self._slide.shapes:
             left = unit_conversion(shape.left, self._measurement_unit)
             top = unit_conversion(shape.top, self._measurement_unit)
@@ -32,36 +41,49 @@ class MarginWhiteSpaceScorer:
             rightmost_x = max(rightmost_x, right)
             topmost_y = min(topmost_y, top)
             bottommost_y = max(bottommost_y, bottom)
+
         return {
-            'left': leftmost_x,
-            'right': rightmost_x,
-            'top': topmost_y,
-            'bottom': bottommost_y
+            "left": leftmost_x,
+            "right": rightmost_x,
+            "top": topmost_y,
+            "bottom": bottommost_y,
         }
-        
+
     def _calculate_margins(self) -> dict:
         bounding_box = self._get_bounding_box()
-        left_margin = bounding_box['left']
-        right_margin = self._width - bounding_box['right']
-        top_margin = bounding_box['top']
-        bottom_margin = self._height - bounding_box['bottom']
+        left_margin = bounding_box["left"]
+        right_margin = self._width - bounding_box["right"]
+        top_margin = bounding_box["top"]
+        bottom_margin = self._height - bounding_box["bottom"]
         return {
-            'left': left_margin,
-            'right': right_margin,
-            'top': top_margin,
-            'bottom': bottom_margin
+            "left": left_margin,
+            "right": right_margin,
+            "top": top_margin,
+            "bottom": bottom_margin,
         }
-    
+
     def calculate_white_space_score(self) -> float:
         margins = self._calculate_margins()
         horizontal_margin_score = 0
         vertical_margin_score = 0
-        if margins['left'] > self._margin_threshold['horizontal'][0] and margins['left'] < self._margin_threshold['horizontal'][1]:
+        if (
+            margins["left"] >= self._margin_threshold["horizontal"][0]
+            and margins["left"] <= self._margin_threshold["horizontal"][1]
+        ):
             horizontal_margin_score += 1
-        if margins['right'] > self._margin_threshold['horizontal'][0] and margins['right'] < self._margin_threshold['horizontal'][1]:
+        if (
+            margins["right"] >= self._margin_threshold["horizontal"][0]
+            and margins["right"] <= self._margin_threshold["horizontal"][1]
+        ):
             horizontal_margin_score += 1
-        if margins['top'] > self._margin_threshold['vertical'][0] and margins['top'] < self._margin_threshold['vertical'][1]:
+        if (
+            margins["top"] >= self._margin_threshold["vertical"][0]
+            and margins["top"] <= self._margin_threshold["vertical"][1]
+        ):
             vertical_margin_score += 1
-        if margins['bottom'] > self._margin_threshold['vertical'][0] and margins['bottom'] < self._margin_threshold['vertical'][1]:
+        if (
+            margins["bottom"] >= self._margin_threshold["vertical"][0]
+            and margins["bottom"] <= self._margin_threshold["vertical"][1]
+        ):
             vertical_margin_score += 1
         return float((horizontal_margin_score + vertical_margin_score) / 4)
